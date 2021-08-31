d02336396859f24051d1d4fc7534032dcce1db8f
Girish Katta <girishkumar9@gmail.com>
2021-08-31 00:57:55 -0500

    Added packages and main application class

---
+++ src/main/java/com/target/spring/products/ProductsSpringBootApplication.java
@@ -0,0 +1,11 @@
+package com.target.spring.products;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductsSpringBootApplication {
+    public static void main(String[] args) {
+		SpringApplication.run(ProductsSpringBootApplication.class, args);
+	}
+}
